9011e9358e28973b2c6bcebe71d6c4d5fe0fe456
VirtualPirate <artaza13020artaza@gmail.com>
2020-09-25 12:48:37 +0530

    Implemented Constant to Constant arithmetic operation functions signatures

---
+++ Operation_Functions.hpp
@@ -0,0 +1,11 @@
+#ifndef Operation_Functions_H
+#define Operation_Functions_H
+
+//Constant to Constant arithmetic operators
+Constant Constant_Constant_add(const Constant&, const Constant&);
+Constant Constant_Constant_sub(const Constant&, const Constant&);
+Constant Constant_Constant_mul(const Constant&, const Constant&);
+Constant Constant_Constant_div(const Constant&, const Constant&);
+Constant Constant_Constant_pow(const Constant&, const Constant&);
+
+#endif
