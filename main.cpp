438f7a882bd178ebe644d0889e71349472804cee
VirtualPirate <artaza13020artaza@gmail.com>
2020-09-25 12:21:37 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+
+#include "Constant.hpp"
+#include "Operand.hpp"
+#include "Variable.hpp"
+#include "Term.hpp"
+#include "Expression.hpp"
+#include "SimpleExpression.hpp"
+
+int main(){
+	std::cout << "This is the initial project version" << std::endl;
+	
+	return 0;
+}
