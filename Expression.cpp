d6941f06af67899ad241ed78e7ec7843eeea0b0e
VirtualPirate <artaza13020artaza@gmail.com>
2020-10-14 16:50:10 +0530

    added Expression::operator<<

--- Expression.cpp
+++ Expression.cpp
@@ -8,6 +8,22 @@
 #include "Expression.hpp"
 #include "Operation_Functions.hpp"
 
+std::ostream& operator<<(std::ostream& os, const Expression& ref){
+	if(!ref.isNull()){
+	const std::vector<Operand>& fields = ref.getFields();
+	os << *(fields.begin());
+	for(auto each=fields.begin()+1; each!=fields.end(); each++)
+		if(!each->is_negative())
+			os << " + " << *each;
+		else
+			os << *each;
+	}
+	else{
+		os << "null_expression{}";
+	}
+	return os;
+}
+
 bool Expression::is_negative() const {
 	return false;
 }
--- Expression.hpp
+++ Expression.hpp
@@ -8,9 +8,11 @@ class Operand;
 class Variable;
 class OperandList;
 class Expression: public OperandList{
+	friend std::ostream& operator<<(std::ostream&, const Expression&);
+public:
 	bool is_negative() const;
 	bool negative_power() const;
-	
+
 };
 
 #endif
