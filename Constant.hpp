4ff1e7f13236c2a68b538ea46d514952994e04a0
VirtualPirate <artaza13020artaza@gmail.com>
2020-10-16 18:51:31 +0530

    Extended the Constant.hpp file

--- Constant.hpp
+++ Constant.hpp
@@ -3,6 +3,10 @@
 
 #include <iostream>
 
+class Operand;
+class Variable;
+class Term;
+class Expression;
 class Constant{
 	double value;
 
@@ -17,10 +21,6 @@ public:
 	Constant& operator=(double);
 	Constant& operator=(const Constant&);
 
-	//Constant to Constant comparison operator
-	bool operator==(const Constant&)const;
-	bool operator!=(const Constant&)const;
-
 	//Getter Methods
 	const double& getValue() const;
 
@@ -32,12 +32,60 @@ public:
 	bool negative_power() const;
 	Constant abs() const;
 
+	//Constant to double arithmetic operations
+	Constant operator+(const double&) const;
+	Constant operator-(const double&) const;
+	Constant operator*(const double&) const;
+	Constant operator/(const double&) const;
+	Constant raise_pow(const double&) const;
+
 	//Constant to Constant operations
 	Constant operator+(const Constant&) const;
 	Constant operator-(const Constant&) const;
 	Constant operator*(const Constant&) const;
 	Constant operator/(const Constant&) const;
 	Constant raise_pow(const Constant&) const;
+
+	//Constant to Variable arithmetic operations
+	Term operator+(const Variable&) const;
+	Term operator-(const Variable&) const;
+	Term operator*(const Variable&) const;
+	Term operator/(const Variable&) const;
+	Term raise_pow(const Variable&) const;
+
+	//Constant to Term arithmetic operations
+	Term operator+(const Term&) const;
+	Term operator-(const Term&) const;
+	Term operator*(const Term&) const;
+	Term operator/(const Term&) const;
+	Term raise_pow(const Term&) const;
+
+	//Constant to Expression arithmetic operations
+	Expression operator+(const Expression&) const;
+	Expression operator-(const Expression&) const;
+	Expression operator*(const Expression&) const;
+	Expression operator/(const Expression&) const;
+	Term raise_pow(const Expression&) const;
+
+
+	bool operator==(const double&) const;
+	bool operator!=(const double&) const;
+
+	bool operator==(const Constant&)const;
+	bool operator!=(const Constant&)const;
+
+	bool operator==(const Variable&) const;
+	bool operator!=(const Variable&) const;
+
+	bool operator==(const Term&) const;
+	bool operator!=(const Term&) const;
+
+	bool operator==(const Expression&) const;
+	bool operator!=(const Expression&) const;
+
+	bool operator==(const Operand&) const;
+	bool operator!=(const Operand&) const;
+
 
 	//Unary operators 
 	Constant operator+() const;
