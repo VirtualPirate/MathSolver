8ae99f9f56fad0dd847442a5f672370e5f3e6673
VirtualPirate <artaza13020artaza@gmail.com>
2020-12-16 17:20:01 +0530

    Added Substitutor_List class

--- Substitutor.hpp
+++ Substitutor.hpp
@@ -2,6 +2,7 @@
 #define Substitutor_H
 
 #include "Operand.hpp"
+#include <vector>
 
 class Operand;
 
@@ -19,10 +20,23 @@ public:
 	const Operand& getKey() const;
 };
 
-/*class Substitutor_List{
+class Substitutor_List{
 	std::vector<Substitutor> sub_;
 public:
-	Substitutor_List(std::initializer_list<Substitutor>)
-}*/
+	Substitutor_List(std::initializer_list<Substitutor>);
+	Substitutor_List& operator=(std::initializer_list<Substitutor>);
+
+	Substitutor_List(const std::vector<Substitutor>&);
+	Substitutor_List& operator=(const std::vector<Substitutor>&);
+
+	// Substitutor List modifier
+	void append(Substitutor);
+	void extend(std::initializer_list<Substitutor>);
+	void extend(std::vector<Substitutor>);
+
+	void erase(size_t index);
+	void erase(const Substitutor&);
+	void clear();
+};
 
 #endif
